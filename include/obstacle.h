875bc74e50eee9569c2c7637ca0c403e3d71a838
Shon Cortes <scortes3@umd.edu>
2021-10-10 17:09:27 -0700

    Created skelleton for obstacle class.

---
+++ include/obstacle.h
@@ -0,0 +1,29 @@
+/** Copyright 2021
+ *  @Authors
+ *  Shon Cortes & Sameer Pusegaonkar
+*/
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Obstacle {
+ private:
+  std::string label_;
+  float camera_x_position_;
+  float camera_z_position_;
+  float robot_x_position_;
+  float robot_y_position_;
+  float human_height_;
+  int obstacle_width_;
+  int obstacle_height_;
+
+ public:
+  void ComputeDepth(float focal_length);
+  void ComputeHorizontalPosition();
+  std::vector<Obstacle> GetRobotFrameCoordinates(std::vector<Obstacle> obstacle,
+   std::vector<int> transformation_matrix);
+  Obstacle::~Obstacle() {}
+};
