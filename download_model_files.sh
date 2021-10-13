32c3d488c2cf0e90641e45c01ca71486bd7d9cb0
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-13 17:38:08 -0400

    A1.9 Add a script to download mobilenet files

---
+++ download_model_files.sh
@@ -0,0 +1,4 @@
+mkdir model_files
+cd model_files
+wget --no-check-certificate 'https://drive.google.com/file/d/1u-7oemk4rMXJ9winpiC4ol8sh75oDV_R/view?usp=sharing' -O MobileNetSSD_deploy.caffemodel
+wget --no-check-certificate 'https://drive.google.com/file/d/1PrAjxI4jruRh5Tk0uQYY1sA3_3wemLlr/view?usp=sharing' -O MobileNetSSD_deploy.prototxt
