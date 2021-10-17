5381504a68dc532e705e2b740ec49c0fc2684816
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-16 22:54:48 -0400

    A3.6 Fix the links for the model & test files

--- download_model_files.sh
+++ download_model_files.sh
@@ -1,4 +1,11 @@
 mkdir model_files
 cd model_files
-wget --no-check-certificate 'https://drive.google.com/file/d/1u-7oemk4rMXJ9winpiC4ol8sh75oDV_R/view?usp=sharing' -O MobileNetSSD_deploy.caffemodel
-wget --no-check-certificate 'https://drive.google.com/file/d/1PrAjxI4jruRh5Tk0uQYY1sA3_3wemLlr/view?usp=sharing' -O MobileNetSSD_deploy.prototxt
+wget -O ./MobileNetSSD_deploy.caffemodel "https://www.dropbox.com/s/w531w3qhfknr0dl/MobileNetSSD_deploy.caffemodel?dl=1"
+wget -O ./MobileNetSSD_deploy.prototxt "https://www.dropbox.com/s/fn9rh3mdpu0u613/MobileNetSSD_deploy.prototxt?dl=1"
+cd ..
+#Download Testing Data
+cd test
+wget -O ./Images.zip "https://www.dropbox.com/s/wwx8tvvfyiefg8g/Images.zip?dl=1"
+sudo apt-get install unzip
+unzip Images.zip
+wget -O ./test_annotation.json "https://www.dropbox.com/s/52ob56vr9eqg8fw/test_annotation.json?dl=1"
