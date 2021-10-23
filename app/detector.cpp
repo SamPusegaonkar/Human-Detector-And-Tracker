b6821d30181a130b55c2362e3c1356182c4e1f15
Sameer Pusegaonkar <sameer@umd.edu>
2021-10-23 15:50:22 -0400

    B3.4 Cleanup code linting errors

--- app/detector.cpp
+++ app/detector.cpp
@@ -7,12 +7,13 @@
  * @copyright Copyright (c) 2021
  */
 
+
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
-#include "../include/detector.h"
 #include "../include/camera.h"
+#include "../include/detector.h"
 
 /**
  * @brief Load the human detection model.
@@ -23,6 +24,7 @@
 bool Detector::LoadModel(std::string file_name) {
   std::string model_file_binary = file_name + ".caffemodel";
   std::string model_file_text = file_name + ".prototxt";
+  this->model_file_ = file_name;
   return std::ifstream(model_file_binary) && std::ifstream(model_file_text);
 }
 
@@ -31,20 +33,21 @@ bool Detector::LoadModel(std::string file_name) {
  * @param img 
  * @return std::vector<std::vector<double>> 
  */
-std::vector<std::vector<double>> Detector::Detect(cv:: Mat img) {
+std::vector<std::vector<double>> Detector::Detect(cv::Mat img) {
   // Gets all the bounding boxes
   auto detections = this->GetBoundingBoxes(img);
   auto obstacles = this->DefineObstacles(detections);
   std::vector<std::vector<double>> positions;
 
-  for ( auto i = 0; i < detections.size(); i++ ) {
+  for (auto i = 0; i < detections.size(); i++) {
     auto obstacle = obstacles[i];
     auto detection = detections[i];
 
     obstacle.ComputeDepth(cam_.focal_length_);
     obstacle.ComputeHorizontalPosition();
 
-    auto robot_pos = obstacle.GetRobotFrameCoordinates(cam_.transformation_matrix_);
+    auto robot_pos = obstacle.GetRobotFrameCoordinates(
+        cam_.transformation_matrix_);
     positions.push_back(robot_pos);
 
     int box_x = detection[0];
@@ -55,7 +58,10 @@ std::vector<std::vector<double>> Detector::Detect(cv:: Mat img) {
     cv::rectangle(img, cv::Point(box_x, box_y),
       cv::Point(box_x + box_width, box_y + box_height),
       cv::Scalar(255, 255, 255), 2);
-    std::string text = "Human x: " + std::to_string(robot_pos[0]) + "y: " + std::to_string(robot_pos[1]);
+
+    std::string text = "Human x: " +
+      std::to_string(robot_pos[0]) +
+      "y: " + std::to_string(robot_pos[1]);
 
     cv::putText(img, text, cv::Point(box_x, box_y - 5),
       cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 255), 1);
@@ -72,8 +78,8 @@ std::vector<std::vector<double>> Detector::Detect(cv:: Mat img) {
  */
 std::vector<std::vector<int>> Detector::GetBoundingBoxes(cv::Mat img) {
   auto model =
-    cv::dnn::readNetFromCaffe("../model_files/MobileNetSSD_deploy.prototxt",
-    "../model_files/MobileNetSSD_deploy.caffemodel");
+    cv::dnn::readNetFromCaffe(this->model_file_ + ".prototxt",
+                              this->model_file_ + ".caffemodel");
 
   std::vector<std::vector<int>> detections;
   int image_height = img.cols;
@@ -95,42 +101,42 @@ std::vector<std::vector<int>> Detector::GetBoundingBoxes(cv::Mat img) {
                         CV_32F, output.ptr<float>());
 
   for (int i = 0; i < detectionMat.rows; i++) {
-    int class_id = detectionMat.at<float>(i, 1);
-    float confidence = detectionMat.at<float>(i, 2);
+      int class_id = detectionMat.at<float>(i, 1);
+      float confidence = detectionMat.at<float>(i, 2);
 
-    // Check if the detection is of good quality
-    if (confidence > 0.4) {
-      int box_x =
-        static_cast<int>(detectionMat.at<float>(i, 3) * img.cols);
+      // Check if the detection is of good quality
+      if (confidence > this->confidence_) {
+          int box_x =
+              static_cast<int>(detectionMat.at<float>(i, 3) * img.cols);
 
-      int box_y =
-        static_cast<int>(detectionMat.at<float>(i, 4) * img.rows);
+          int box_y =
+              static_cast<int>(detectionMat.at<float>(i, 4) * img.rows);
 
-      int box_width =
-        static_cast<int>(detectionMat.at<float>(i, 5) * img.cols - box_x);
+          int box_width =
+              static_cast<int>(detectionMat.at<float>(i, 5) * img.cols - box_x);
 
-      int box_height =
-        static_cast<int>(detectionMat.at<float>(i, 6) * img.rows - box_y);
+          int box_height =
+              static_cast<int>(detectionMat.at<float>(i, 6) * img.rows - box_y);
 
-      detections.push_back({box_x, box_y, box_width, box_height});
-    }
+          detections.push_back({box_x, box_y, box_width, box_height});
+      }
   }
   return detections;
 }
 
-std::vector<Obstacle> Detector::DefineObstacles(std::vector<std::vector<int>> coordinates) {
-  std::vector<Obstacle> obstacles(coordinates.size());
+std::vector<Obstacle> Detector::DefineObstacles(
+    std::vector<std::vector<int>> coordinates) {
+    std::vector<Obstacle> obstacles(coordinates.size());
 
-  for ( int i = 0; i < obstacles.size(); i++ ) {
-    auto coordinate = coordinates[i];
-    int box_width = coordinate[2];
-    int box_height = coordinate[3];
+    for (int i = 0; i < obstacles.size(); i++) {
+        auto coordinate = coordinates[i];
+        int box_width = coordinate[2];
+        int box_height = coordinate[3];
 
-
-    obstacles[i].SetObstacleWidth(box_width);
-    obstacles[i].SetObstacleHeight(box_height);
-  }
-  return obstacles;
+        obstacles[i].SetObstacleWidth(box_width);
+        obstacles[i].SetObstacleHeight(box_height);
+    }
+    return obstacles;
 }
 
 /**
@@ -139,7 +145,7 @@ std::vector<Obstacle> Detector::DefineObstacles(std::vector<std::vector<int>> co
  * @param frame Video frame being processed.
  * @return cv::Mat Video frame with coordinates displayed.
  */
-cv::Mat Detector::WriteRobotCoordinatesOnFrame(cv::Mat& frame ) {
-  cv::imshow("Video feed from Medibot", frame);
-  return frame;
+cv::Mat Detector::WriteRobotCoordinatesOnFrame(const cv::Mat& frame) {
+    cv::imshow("Video feed from Medibot", frame);
+    return frame;
 }
