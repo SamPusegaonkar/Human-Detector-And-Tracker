08f612adac39a836ac264165cf6e6ff0fe1b84ab
Shon Cortes <scortes3@umd.edu>
2021-10-23 20:41:10 -0700

    B3.4 Code cleanup

--- run_valgrind.sh
+++ run_valgrind.sh
@@ -1,2 +1,3 @@
-valgrind --leak-check=full --log-file=results/valgrind_results.txt ./build/app/shell-app
+cd build 
+valgrind --leak-check=full --log-file=../results/valgrind_results.txt ./app/shell-app
 echo "Done Processing. Results are stored in results/valgrind_results.txt"
